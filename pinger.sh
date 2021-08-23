6fafa8fda672acd7a85c1033af229b27c98cf694
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-23 00:28:34 -0500

    fixing pinger.sh addresses

--- pinger.sh
+++ pinger.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 current_date=$(date)
-server0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.13:4000/status)
-server1_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.16:4000/status)
+server0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://172.18.0.3:8000/status)
+server1_ping=$(curl -s -o /dev/null -w '%{http_code}' http://172.18.0.4:8000/status)
 
 echo "$server0_ping $current_date" > /home/debian/last_server0_ping.txt
 echo "$server1_ping $current_date" > /home/debian/last_server1_ping.txt
