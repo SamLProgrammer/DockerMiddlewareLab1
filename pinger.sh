03e3469de5183dc25ec493d5f9eef2d67a645f8c
Samuel López <samuel.lopez@uptc.edu.co>
2021-08-23 00:46:12 -0500

    pinger.sh echo correction

--- pinger.sh
+++ pinger.sh
@@ -3,5 +3,5 @@ current_date=$(date)
 server0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://172.18.0.3:8000/status)
 server1_ping=$(curl -s -o /dev/null -w '%{http_code}' http://172.18.0.4:8000/status)
 
-echo "$server0_ping $current_date" > /home/debian/last_server0_ping.txt
-echo "$server1_ping $current_date" > /home/debian/last_server1_ping.txt
+echo "$server0_ping $current_date" > /home/debian/DockerMiddlewareLab1/last_server0_ping.txt
+echo "$server1_ping $current_date" > /home/debian/DockerMiddlewareLab1/last_server1_ping.txt
